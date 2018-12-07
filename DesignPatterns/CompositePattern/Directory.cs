7611cdcaeed84bf23f6a342ab0926c678d1a4f5a
BraddockG <braddockgerome@gmail.com>
2018-12-07 17:16:07 +0000

    First commit for the Composite pattern

---
+++ DesignPatterns/CompositePattern/Directory.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CompositePattern
+{
+    class Directory : IFile
+    {
+        private string directoryName;
+        private List<IFile> includedFiles = new List<IFile>();
+        private string indentString = "----";
+
+
+        public Directory(string directoryName)
+        {
+            this.directoryName = directoryName;
+        }
+
+        public void InsertFile(IFile fileToInsert)
+        {
+            includedFiles.Add(fileToInsert);
+        }
+
+        public void InsertFiles(List<IFile> filesToInsert)
+        {
+            foreach(IFile file in filesToInsert)
+            {
+                InsertFile(file);
+            }
+        }
+
+        public void RemoveFile(IFile fileToRemove)
+        {
+            includedFiles.Remove(fileToRemove);
+        }
+
+        public void ListFiles()
+        {
+            Console.WriteLine(directoryName);
+
+            Client.fileIndents += indentString;
+
+            foreach (IFile file in includedFiles)
+            {
+                Console.Write(Client.fileIndents);
+                file.ListFiles();
+            }
+
+            RemoveIndentString();
+        }
+
+        private void RemoveIndentString()
+        {
+            Client.fileIndents = Client.fileIndents.Remove(Client.fileIndents.Length - indentString.Length);
+        }
+    }
+}
---
+++ DesignPatterns/CompositePattern/File.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompositePattern
+{
+    class File : IFile
+    {
+        private string fileName;
+
+        public File(string fileName)
+        {
+            this.fileName = fileName;
+        }
+
+        public void ListFiles()
+        {
+            Console.WriteLine(fileName);
+        }
+    }
+}
