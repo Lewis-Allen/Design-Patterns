e8e448051d5812daae78ac1fbc6a930acde1869f
Lewis-Allen <lewisn11@hotmail.co.uk>
2019-01-16 13:32:15 +0000

    Added strategy pattern

---
+++ DesignPatterns/StrategyPattern/CardPayment.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace StrategyPattern
+{
+    class CardPayment : IPaymentStrategy
+    {
+        private string cardNo;
+
+        public CardPayment(string cardNo)
+        {
+            this.cardNo = cardNo;
+        }
+
+        public void MakePayment(double amount)
+        {
+            Console.WriteLine($"Made a payment of £{amount} using credit card no: {cardNo}");
+        }
+    }
+}
---
+++ DesignPatterns/StrategyPattern/Product.cs
@@ -0,0 +1,24 @@
+﻿namespace StrategyPattern
+{
+    class Product
+    {
+        private double price;
+        private string name;
+
+        public Product(double price, string name)
+        {
+            this.price = price;
+            this.name = name;
+        }
+
+        public string GetName()
+        {
+            return this.name;
+        }
+
+        public double GetPrice()
+        {
+            return this.price;
+        }
+    }
+}
