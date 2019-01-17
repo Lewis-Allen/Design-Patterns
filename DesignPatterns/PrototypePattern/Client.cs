d0e46006cc3f4828a1ce282df2a058148883e176
Lewis-Allen <lewisn11@hotmail.co.uk>
2019-01-17 18:08:51 +0000

    Implemented Prototype Pattern

---
+++ DesignPatterns/PrototypePattern/Client.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrototypePattern
+{
+    public class Client
+    {
+        public static void Main()
+        {
+            Prototype m = new Monster(10, 3, 2, 9, 9, 0.7);
+
+            Prototype clone = m.Clone();
+            Console.WriteLine(clone.ToString());
+        }
+    }
+}
---
+++ DesignPatterns/PrototypePattern/Monster.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrototypePattern
+{
+    class Monster : Prototype
+    {
+        private int health,
+                    age,
+                    range,
+                    attack,
+                    defense;
+
+        private double value = 0.49;
+
+        public Monster(int health, int age, int range, int attack, int defense, double value)
+        {
+            this.health = health;
+            this.age = age;
+            this.range = range;
+            this.attack = attack;
+            this.defense = defense;
+            this.value = value;
+        }
+
+        public Prototype Clone()
+        {
+            Prototype clone = new Monster(health, age, range, attack, defense, value);
+            
+            return clone;
+        }
+
+        public override string ToString()
+        {
+            return $"Health:{health}, Age:{age}, Range:{range}, Attack:{attack}, Defense:{attack}, Value:{value}.";
+        }
+    }
+}
