c1987d0ff6fae286908ceca98a0d819f8238a485
petermcneil <peter.john.mcneil@gmail.com>
2019-01-18 11:27:55 +0000

    Cleaned up builder pattern.

--- DesignPatterns/BuilderPattern/Car.cs
+++ DesignPatterns/BuilderPattern/Car.cs
@@ -2,27 +2,26 @@ namespace BuilderPattern
 {
     public class Car
     {
-        private readonly string Colour;
-        private readonly int Wheels;
-        private readonly string Name;
+        private readonly string _colour;
+        private readonly int _wheels;
+        private readonly string _name;
 
         private Car(CarBuilder from)
         {
-            this.Colour = from.Colour;
-            this.Wheels = from.Wheels;
-            this.Name = from.Name;
+            this._colour = from.Colour;
+            this._wheels = from.Wheels;
+            this._name = from.Name;
         }
 
         public override string ToString()
         {
-            return $"This {this.Colour} car has {this.Wheels} wheel(s) and is called {this.Name}.\n";
+            return $"This {this._colour} car has {this._wheels} wheel(s) and is called {this._name}.\n";
         }
 
         public static CarBuilder Builder()
         {
             return new CarBuilder();
         }
-
 
         public class CarBuilder
         {
